2ebc6a7b4c09496b7adc70bc011e0e05f0d095a3
Christopher Belmonte <christopherbelmo@yahoo.com>
2014-01-30 18:35:26 -0500

    Lab 3 -  Getting Started With Sequel Queries
    Completed Lab 3.

---
+++ Lab3 - Getting Started With SQL Queries.sql
@@ -0,0 +1,21 @@
+﻿/* Lab 3 - Completed on January 30 */
+
+select name, city from agents where name = 'Smith';
+
+select pid, name, quantity from products where (priceusd > 1.25);
+
+select ordno, aid from orders;
+
+select name, city from customers where city = 'Dallas';
+
+select name from agents where (city != 'New York') and (city !=  'Newark');
+
+select * from products where (city != 'New York') and (city != 'Newark') and (priceusd > 1.00);
+
+select * from orders where (mon = 'jan') or (mon = 'mar');
+
+select * from orders where (mon = 'feb') and (dollars < 100);
+/*There is none? */
+
+select * from orders where cid = 'c001';
+
