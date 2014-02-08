8ee9ff82a946cb701f0b675052675267661b4e21
Christopher Belmonte <christopherbelmo@yahoo.com>
2014-02-08 12:10:04 -0500

    Lab 4 - SQL Queries The Sequel
    Lab 4 very close to complete. I want to play with #7 some more and
    figure out why it won't show me the name and discount. It gives blank
    tables, but without the name I get the correct values. Weird.

---
+++ Lab4 - SQL Queries The Sequel.sql
@@ -0,0 +1,76 @@
+﻿-- 1. Get the cities of agents booking an order for customer “Basics”
+select city
+from agents
+where aid in (select aid
+		from orders
+		where cid in (select cid
+				from customers
+				where name = 'Basics')
+	     )
+order by city asc;
+
+-- 2. Get the pids of products ordered	through	any agent 
+-- who makes at least one order for a customer in Kyoto.
+select distinct pid
+from products
+where pid in (select pid
+		from orders
+		where cid in (select cid
+				from customers
+				where city = 'Kyoto')
+		)		
+order by pid asc;	
+
+-- 3. Find the cids and	names of customers who never placed an order through agent a03.
+select distinct cid, name
+from customers
+where cid in (select cid
+		from orders
+		where aid != 'a03'
+		)
+order by cid asc;
+
+-- 4. Get the cids and names of	customers who ordered both product p01 and p07.
+select distinct cid, name
+from customers
+where cid in (select cid
+		from orders
+		where pid = 'p01'
+			or pid = 'p07')
+order by cid asc;
+
+-- 5. Get the pids of products ordered by any customers	
+--who ever placed an order through agent a03.
+select distinct pid
+from orders 
+where aid in (select aid
+		from orders
+		where aid = 'a03')	       
+order by pid asc;
+
+-- 6. Get the names and	discounts of all customers who	
+-- place orders	through	agents in Dallas or Duluth.
+select distinct name, discount
+from customers
+where cid in (select cid
+		from orders
+		where aid in (select aid
+				from agents
+				where city = 'Dallas'
+				or city = 'Duluth')
+		)
+order by name asc;
+
+-- 7. Find all customers who have the same discount as that of any customers in Dallas or Kyoto.
+select discount
+from customers
+where discount in (select discount
+			from customers
+			where city = 'Dallas'
+			or city = 'Kyoto'
+			)
+group by discount
+having count(discount) >= 2;
+
+
+
